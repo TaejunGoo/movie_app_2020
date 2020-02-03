3e4b1ac37cc65c679f16b164d6ce721f96fd14c1
TaejunGoo <gtx7@ileadon.com>
2020-02-03 20:22:22 +0900

    JSX 배우기

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
 
+function Food({fav}) {
+  console.log(fav)
+  return (
+    <h2>이건 {fav} 컴포넌트지</h2>
+  )
+}
+
 function App() {
   return (
-    <div><h1>Hello React JS!</h1></div>
+    <div>
+      <h1>Hello React JS!</h1>
+      <p>이건 토마토 컴포넌트야</p>
+      <Food fav="치킨" />
+      <Food fav="국밥" />
+      <Food fav="샐러드" />
+    </div>
   );
 }
 
