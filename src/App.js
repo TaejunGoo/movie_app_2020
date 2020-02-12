8d6a5cb2b765270bd92aaded847849701e116697
TaejunGoo <gtx7@ileadon.com>
2020-02-12 21:20:55 +0900

    class와 extends를 통한 컴포넌트 생성과 render, state, setState를 통한 동적 데이터 조작

--- src/App.js
+++ src/App.js
@@ -1,116 +1,32 @@
 import React from 'react';
 import PropTypes from "prop-types";
 
-function JustTest({id, nName, word}){
-  return(
-    <div>
-      <p> {nName}({id}) say "{word}" </p>
+class App extends React.Component{
+  state = {
+    count : 0
+  };
+  // 동적 데이터
+
+  add = () => {
+    this.setState({count: this.state.count + 1 })
+    // = 를 이용하여 데이터를 할당하는것이 아니라(state를 직접 조작하는것이 아니라!)
+    // setState를 이용하여 state를 조작해야 자동으로 react가 refresh 된다.
+    // 그러면 react가 자동으로 변경 부분만 html을 refresh
+  };
+  minus = () => {
+    this.setState(current => ({ count: current.count - 1}))
+    // 위의 this.state.count를 활용하는 방법보다 효율적이다
+    // setState를 할 떄 외부의 상태에 의존하지 않아야?
+  };
+  render(){
+    return (
+    <div>    
+      <h1>지금 숫자는 {this.state.count} 이다 </h1>
+      <button onClick={this.add}>더하기</button>
+      <button onClick={this.minus}>빼기</button>
     </div>
-  )
-}
-
-function Food({fav, pic, rating}) {
-  return (
-    <div>
-      <h2>이건 {fav} 컴포넌트지</h2>
-      <h3>{rating}점</h3>
-      <img src={pic} alt={fav}/>
-    </div>
-  )
-}
-
-function MovieChart({sbj, genre, ratings}) {
-  return (
-    <div>
-      <p>Subject : {sbj}</p>
-      <p>genre : {genre}</p>
-      <p>rating : {ratings}/5</p>
-    </div>
-  )
-}
-
-Food.propTypes = {
-  fav: PropTypes.string.isRequired,
-  pic: PropTypes.string,
-  rating: PropTypes.number.isRequired
-}
-
-MovieChart.propTypes = {
-  sbj: PropTypes.string.isRequired,
-  ratings: PropTypes.number
-}
-// propTypes를 이용하여 각 prop이 어떤 형태로 제공되어야하는지 정의 할 수 있다
-
-const foodIlike = [
-  {
-    id : 1,
-    name : "beer",
-    image : "https://produits.bienmanger.com/34491-0w600h600_Corona_Extra_Mexican_Blonde_Beer.jpg",
-    rating : 4
-  },
-  {
-    id : 2,
-    name : "chicken",
-    image : "https://cdn.apartmenttherapy.info/image/fetch/f_jpg,q_auto:eco,c_fill,g_auto,w_1500,ar_4:3/https%3A%2F%2Fstorage.googleapis.com%2Fgen-atmedia%2F3%2F2017%2F04%2F0716e0ca369a57b31b821ea090891e90fce7123e.jpeg",
-    rating : 4.9
-  },
-  {
-    id : 3,
-    name : "fried rice",
-    image : "https://www.fifteenspatulas.com/wp-content/uploads/2012/03/Fried-Rice-Fifteen-Spatulas-8-640x427.jpg",
-    rating : 3
+    )
   }
-]
-
-const users = [
-  {
-    id : "gtx1",
-    name : "nick",
-    word : "Hi!"
-  },
-  {
-    id : "gtxggle",
-    name : "태준",
-    word : "안녕하세요"
-  },
-  {
-    id : "kim",
-    name : "김이박",
-    word : "안녕하슈"
-  }
-]
-
-const movies = [
-  {
-    sbj : "Lord of Rings",
-    genre : "fantasy",
-    ratings : 5
-  },
-  {
-    sbj : "Star Wars Series",
-    genre : "space opera",
-    ratings : 3
-  },
-  {
-    sbj: "kung ju soccer",
-    genre : "comedy",
-    ratings : 5
-  },
-  {
-    sbj : "Gravity",
-    genre : "Science Fiction",
-    ratings : 5
-  }
-]
-
-function App() {
-  return (
-    <div>
-      {foodIlike.map(dish => <Food key={dish.id} fav={dish.name} pic={dish.image} rating={dish.rating} />)}
-      {users.map( people => <JustTest key={people.id} id={people.id} nName={people.name} word={people.word} /> )}
-      {movies.map(title => <MovieChart key={title.sbj} sbj={title.sbj} genre={title.genre} ratings={title.ratings} />)}
-    </div>
-  );
 }
 
 export default App;
