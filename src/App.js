369130f039c52d818aaa92fc723363a8170d5be4
TaejunGoo <gtx7@ileadon.com>
2020-02-16 21:25:21 +0900

    Make Movie List App

--- src/App.js
+++ src/App.js
@@ -1,22 +1,54 @@
 import React from 'react';
-import PropTypes from "prop-types";
+import axios from "axios";
+import Movie from "./Movie"
+import "./App.css"
 
 class App extends React.Component{
   state = {
     isLoading : true,
     movies: []
   };
+  getMoives = async () => {
+    const {
+      data: {
+        data : {movies}
+      }
+    } = await axios.get("https://yts.mx/api/v2/list_movies.json?sort_by=rating");
+    this.setState({ movies, isLoading: false });
+    console.log(movies)
+  }
   componentDidMount(){
-    setTimeout(()=> {
-      this.setState({isLoading : false});
-    }, 3000)
+    this.getMoives();
+    
   }
   render() {
-    const { isLoading } = this.state;
+    const {isLoading, movies} = this.state;
     return (
-      <div>
-        {isLoading ? "Loading..." : "We are ready"}
-      </div>
+      <section className="container">
+        {isLoading 
+        ? <div className="loader">
+          <span className="loader_text">Loading...</span>
+        </div> 
+        : 
+          <div className="movies">
+            {
+              movies.map(movie => {
+                return (
+                      <Movie 
+                      key={movie.id}
+                            id={movie.id} 
+                            year={movie.year} 
+                            title={movie.title} 
+                            summary={movie.summary} 
+                            poster={movie.medium_cover_image} 
+                            genres={movie.genres}
+                      />
+                      )
+              })
+            }
+          </div>
+        }
+      </section>
     )
   }
 }
